883fe159e63174245efeae30b0327f17f0cec844
Reggio <contact@graphandco.com>
2021-05-24 18:26:27 +0200

    Add FirebaseUI Auth

--- src/components/Realisations/UpdateDelete.js
+++ src/components/Realisations/UpdateDelete.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import firebase from "../../utils/firebaseConfig";
 
 const UpdateDelete = ({ item }) => {
