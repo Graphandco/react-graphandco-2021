3abb3d843d9ae2ab8fe34a4a4d1fc231703029a0
Reggio <contact@graphandco.com>
2022-02-21 17:56:43 +0100

    Reset Hero and Réalisations

--- src/components/HomePage/Hero.js
+++ src/components/HomePage/Hero.js
@@ -1,133 +1,63 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Button from '../Button';
 import { gsap } from 'gsap';
 
-// import { ReactComponent as HeroImg } from '../../assets/illustration.svg';
-import { ReactComponent as SiteName } from '../../assets/site-title.svg';
-
+import { ReactComponent as HeroImg } from '../../assets/illustration.svg';
 import TextAnimation from '../animations/TextAnimation';
 
 export const Hero = () => {
-  const svgRef = useRef(null);
-  const contentRef = useRef(null);
+    useEffect(() => {
+        let tl = gsap.timeline({ delay: 0.5 });
+        tl.from('#Clavier', {
+            y: '200px',
+            opacity: 0,
+            ease: 'Power4.easeOut',
+            duration: 0.3,
+        });
+        tl.from('#Woman', {
+            y: '-200px',
+            opacity: 0,
+            ease: 'Power4.easeOut',
+            duration: 0.3,
+        });
+        tl.from('#Man_right', {
+            x: '200px',
+            opacity: 0,
+            ease: 'Power4.easeOut',
+            duration: 0.3,
+        });
+        tl.from('#Man_top', {
+            y: '-200px',
+            opacity: 0,
+            ease: 'Power4.easeOut',
+            duration: 0.3,
+        });
+        tl.from('#Symboles', {
+            opacity: 0,
+            ease: 'Power4.easeOut',
+            duration: 0.8,
+        });
+    }, []);
 
-  function getScaleLogoValue() {
-    if (window.matchMedia('(min-width: 600px)').matches) {
-      return 0.5;
-    } else {
-      return 1;
-    }
-  }
-
-  const createTL = () => {
-    let tl = gsap.timeline({ Defaults: { Easing: 'Expo.EaseOut' } });
-    // tl.from(svgRef.current, {
-    //   y: -150,
-    //   duration: 2,
-    //   scaleY: 0.2,
-    //   // clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
-    // });
-    tl.to(
-      svgRef.current,
-      {
-        clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
-        duration: 2,
-        y: 150,
-      },
-      '+=.3'
+    return (
+        <section className="hero">
+            <div className="hero-wrapper container">
+                <div className="hero-content">
+                    <div className="hero-site-name">Création de sites web</div>
+                    <div className="hero-title">
+                        <span>Un</span> métier<span>, une </span>
+                        <TextAnimation />
+                    </div>
+                    <div className="hero-subtitle">Nous sommes spécialisés dans la réalisation de sites web modernes et intuitifs.</div>
+                    <Button title="Voir nos offres" mt="1" link="/prestations" />
+                </div>
+                <HeroImg />
+                {/* <div className="site-title">
+          <span className="graph">Graph</span>
+          <span className="and">and</span>
+          <span className="co">Co</span>
+        </div> */}
+            </div>
+        </section>
     );
-    tl.to(
-      svgRef.current,
-      {
-        clipPath: 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)',
-        duration: 1,
-      },
-      '+=1'
-    );
-
-    tl.to(svgRef.current, {
-      // clipPath: 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%);',
-      y: 0,
-      duration: 0,
-      width: 'auto',
-      scale: getScaleLogoValue,
-    });
-
-    tl.to(
-      contentRef.current,
-      {
-        clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
-        y: 0,
-        duration: 2,
-      },
-      '-=1.1'
-    );
-    tl.to(
-      svgRef.current,
-      {
-        clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
-        duration: 2,
-      },
-      '+=.5'
-    );
-  };
-
-  useEffect(() => {
-    // if (window.matchMedia('(min-width: 600px)').matches) {
-    //   let ScaleLogoSize = 1;
-    // } else {
-    //   let ScaleLogoSize = 0.5;
-    // }
-    createTL();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-  // useEffect(() => {
-  //   let tl = gsap.timeline({ delay: 0.5 });
-  //   tl.from('#Clavier', {
-  //     y: '200px',
-  //     opacity: 0,
-  //     ease: 'Power4.easeOut',
-  //     duration: 0.3,
-  //   });
-  //   tl.from('#Woman', {
-  //     y: '-200px',
-  //     opacity: 0,
-  //     ease: 'Power4.easeOut',
-  //     duration: 0.3,
-  //   });
-  //   tl.from('#Man_right', {
-  //     x: '200px',
-  //     opacity: 0,
-  //     ease: 'Power4.easeOut',
-  //     duration: 0.3,
-  //   });
-  //   tl.from('#Man_top', {
-  //     y: '-200px',
-  //     opacity: 0,
-  //     ease: 'Power4.easeOut',
-  //     duration: 0.3,
-  //   });
-  //   tl.from('#Symboles', {
-  //     opacity: 0,
-  //     ease: 'Power4.easeOut',
-  //     duration: 0.8,
-  //   });
-  // }, []);
-
-  return (
-    <section className="hero" id="hero">
-      <div className="hero-wrapper container">
-        <SiteName ref={svgRef} />
-        <div className="hero-content" ref={contentRef}>
-          <div className="hero-uptitle">Création de sites web</div>
-          <div className="hero-title">
-            <span>Un</span> métier<span>, une </span>
-            <TextAnimation />
-          </div>
-          <div className="hero-subtitle">Nous sommes spécialisés dans la réalisation de sites web modernes et intuitifs.</div>
-          <Button title="Voir nos offres" mt="1" link="/prestations" />
-        </div>
-      </div>
-    </section>
-  );
 };
--- src/components/Projets/Projects.js
+++ src/components/Projets/Projects.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import { useAuth } from '../../hooks';
 import { CreateProject } from './CreateProject';
 // import { UpdateDeleteProject } from './UpdateDeleteProject';
@@ -6,172 +6,157 @@ import { fb } from '../../services';
 import { ProjectItem } from './ProjectItem';
 import { ReactComponent as RealisationImage } from '../../assets/realisations.svg';
 
-import { motion } from 'framer-motion';
-
 const localData = [
-  {
-    description: 'Exemple de création pour un barbier',
-    name: 'Barber Shop',
-    position: '5',
-    slug: 'barber-shop',
-    url: 'https://barber.graphandco.com',
-    responsive: true,
-    mockup: false,
-  },
-  {
-    description: 'Prototype pour un site de stockage en ligne',
-    name: 'Fylo',
-    position: '1',
-    slug: 'fylo',
-    url: 'https://fylo.graphandco.com',
-    responsive: false,
-    mockup: true,
-  },
-  {
-    description: 'Site pour un magasin de pêche à Kingersheim',
-    name: '3 Frontières Pêche',
-    position: '6',
-    slug: '3fp',
-    url: 'https://3frontierespeche.fr',
-    responsive: true,
-    mockup: false,
-  },
-  {
-    description: 'Site pour un magasin de pêche à Dannemarie',
-    name: 'Pêche Exotique',
-    position: '6',
-    slug: 'peche-exotique',
-    url: 'https://3frontierespeche.fr',
-    responsive: true,
-    mockup: false,
-  },
-  {
-    description: "Exemple de réalisation d'un site pour un restaurant spécialisé dans les burgers",
-    name: 'Burgerio',
-    position: '4',
-    slug: 'burgerio',
-    url: 'https://burgerio.graphandco.com',
-    responsive: false,
-    mockup: true,
-  },
-  {
-    description: 'Site pour le distributeur français des bateaux-amorceurs Boatman',
-    name: 'Boatman',
-    position: '9',
-    slug: 'boatman',
-    url: 'https://www.boatmanfrance.fr',
-    responsive: true,
-    mockup: false,
-  },
-  {
-    description: 'Site pour la pizzeria La Toscana à Mulhouse avec commande et paiement en click and collect.',
-    name: 'La Toscana',
-    position: '2',
-    slug: 'la-toscana',
-    url: 'https://latoscana.fr',
-    responsive: true,
-    mockup: false,
-  },
-  {
-    description: 'Site d’accompagnement à travers la taromancie.',
-    name: 'Willow Tarot',
-    position: '3',
-    slug: 'willow-tarot',
-    url: 'https://willow-tarot.fr',
-    responsive: true,
-    mockup: false,
-  },
-  {
-    description: 'Site vitrine pour un créateur de guitares originales en tissu.',
-    name: 'Loïde Guitare',
-    position: '8',
-    slug: 'loide-guitare',
-    url: 'https://loide-guitare.fr',
-    responsive: true,
-    mockup: false,
-  },
+    {
+        description: 'Exemple de création pour un barbier',
+        name: 'Barber Shop',
+        position: '5',
+        slug: 'barber-shop',
+        url: 'https://barber.graphandco.com',
+        responsive: true,
+        mockup: false,
+    },
+    {
+        description: 'Prototype pour un site de stockage en ligne',
+        name: 'Fylo',
+        position: '1',
+        slug: 'fylo',
+        url: 'https://fylo.graphandco.com',
+        responsive: false,
+        mockup: true,
+    },
+    {
+        description: 'Site pour un magasin de pêche à Kingersheim',
+        name: '3 Frontières Pêche',
+        position: '6',
+        slug: '3fp',
+        url: 'https://3frontierespeche.fr',
+        responsive: true,
+        mockup: false,
+    },
+    {
+        description: 'Site pour un magasin de pêche à Dannemarie',
+        name: 'Pêche Exotique',
+        position: '6',
+        slug: 'peche-exotique',
+        url: 'https://3frontierespeche.fr',
+        responsive: true,
+        mockup: false,
+    },
+    {
+        description: "Exemple de réalisation d'un site pour un restaurant spécialisé dans les burgers",
+        name: 'Burgerio',
+        position: '4',
+        slug: 'burgerio',
+        url: 'https://burgerio.graphandco.com',
+        responsive: false,
+        mockup: true,
+    },
+    {
+        description: 'Site pour le distributeur français des bateaux-amorceurs Boatman',
+        name: 'Boatman',
+        position: '9',
+        slug: 'boatman',
+        url: 'https://www.boatmanfrance.fr',
+        responsive: true,
+        mockup: false,
+    },
+    {
+        description: 'Site pour la pizzeria La Toscana à Mulhouse avec commande et paiement en click and collect.',
+        name: 'La Toscana',
+        position: '2',
+        slug: 'la-toscana',
+        url: 'https://latoscana.fr',
+        responsive: true,
+        mockup: false,
+    },
+    {
+        description: 'Site d’accompagnement à travers la taromancie.',
+        name: 'Willow Tarot',
+        position: '3',
+        slug: 'willow-tarot',
+        url: 'https://willow-tarot.fr',
+        responsive: true,
+        mockup: false,
+    },
+    {
+        description: 'Site vitrine pour un créateur de guitares originales en tissu.',
+        name: 'Loïde Guitare',
+        position: '8',
+        slug: 'loide-guitare',
+        url: 'https://loide-guitare.fr',
+        responsive: true,
+        mockup: false,
+    },
 ];
 
 export const Projects = () => {
-  const { authUser } = useAuth();
-  const [projects, setprojects] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+    const { authUser } = useAuth();
+    const [projects, setprojects] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-  const [width, setWidth] = useState(0);
-  const sliderRef = useRef();
+    useEffect(() => {
+        const unsubscribe = fb.firestore.collection('projects').onSnapshot((snapshot) => {
+            const data = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+            setprojects(data);
+            setIsLoading(false);
+            console.log(projects);
+        });
+        return unsubscribe;
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  useEffect(() => {
-    setWidth(sliderRef.current.scrollWidth - sliderRef.current.offsetWidth);
-  }, []);
-
-  useEffect(() => {
-    const unsubscribe = fb.firestore.collection('projects').onSnapshot((snapshot) => {
-      const data = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setprojects(data);
-      setIsLoading(false);
-      console.log(projects);
-    });
-    return unsubscribe;
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-  //TRI DES PROJETS PAR POSITION
-  const dynamicSort = (property) => {
-    let sortOrder = -1;
-    if (property[0] === '-') {
-      sortOrder = -1;
-      property = property.substr(1);
-    }
-    return function (a, b) {
-      /* next line works with strings and numbers,
-       * and you may want to customize it to your needs
-       */
-      let result = a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
-      return result * sortOrder;
+    //TRI DES PROJETS PAR POSITION
+    const dynamicSort = (property) => {
+        let sortOrder = -1;
+        if (property[0] === '-') {
+            sortOrder = -1;
+            property = property.substr(1);
+        }
+        return function (a, b) {
+            /* next line works with strings and numbers,
+             * and you may want to customize it to your needs
+             */
+            let result = a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
+            return result * sortOrder;
+        };
     };
-  };
-  localData.sort(dynamicSort('position'));
+    localData.sort(dynamicSort('position'));
 
-  return (
-    <>
-      <div className="projects">
-        <div className="projects-description">
-          <div className="projects-description-content">
-            <p>
-              Nous vous présentons les projets réalisés pour les clients qui nous ont fait confiance pour le développement de leur site internet. Qu'il s'agisse
-              d'une refonte ou d'une première présence web, nous avons à coeur de leur livrer <strong>un site qui correspond à leur besoin</strong>, à leur
-              identité, ainsi qu'à leur budget. Nous travaillons également en collaboration avec des référenceurs, graphistes et photographes pour donner vie à
-              un projet dans sa globalité.
-            </p>
-            <p>
-              N'hésitez pas à consulter également les prototypes - ou <strong>"mockups"</strong>- de sites, qui sont des inspirations sur différents thèmes.
-            </p>
-          </div>
-          <RealisationImage />
-        </div>
-        <motion.div ref={sliderRef} whileTap={{ cursor: 'grabbing' }} className="projects-slider">
-          <motion.div drag="x" dragConstraints={{ right: 0, left: -width }} className="projects-slider-inner">
-            {localData.map((project) => (
-              <ProjectItem key={project.slug} project={project} isLoading={isLoading} />
-            ))}
-          </motion.div>
-        </motion.div>
-        {/* <div className="projects-list">
-          <div className="project-item description">
-            <img src="/img/projects/project-lead.png" alt="" />
-            <div className="content">
-              <h2>Portfolio</h2>
-              <p>Retrouvez ici les différents projets que nous avons pu développer pour nos clients.</p>
+    return (
+        <>
+            <div className="projects container">
+                <div className="projects-description">
+                    <div className="projects-description-content">
+                        <p>
+                            Nous vous présentons les projets réalisés pour les clients qui nous ont fait confiance pour le développement de leur site internet.
+                            Qu'il s'agisse d'une refonte ou d'une première présence web, nous avons à coeur de leur livrer{' '}
+                            <strong>un site qui correspond à leur besoin</strong>, à leur identité, ainsi qu'à leur budget. Nous travaillons également en
+                            collaboration avec des référenceurs, graphistes et photographes pour donner vie à un projet dans sa globalité.
+                        </p>
+                        <p>
+                            N'hésitez pas à consulter également les prototypes - ou <strong>"mockups"</strong>- de sites, qui sont des inspirations sur
+                            différents thèmes.
+                        </p>
+                    </div>
+                    <RealisationImage />
+                </div>
+                <div className="projects-list">
+                    <div className="project-item description">
+                        <img src="/img/projects/project-lead.png" alt="" />
+                        <div className="content">
+                            {/* <h2>Portfolio</h2> */}
+                            <p>Retrouvez ici les différents projets que nous avons pu développer pour nos clients.</p>
+                        </div>
+                    </div>
+                    {localData.map((project) => (
+                        <ProjectItem key={project.slug} project={project} isLoading={isLoading} />
+                    ))}
+                </div>
+                {authUser && <CreateProject />}
             </div>
-          </div>
-          {localData.map((project) => (
-            <ProjectItem key={project.slug} project={project} isLoading={isLoading} />
-          ))}
-        </div> */}
-        {authUser && <CreateProject />}
-      </div>
-    </>
-  );
+        </>
+    );
 };
